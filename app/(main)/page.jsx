0ed06e89f3db52f646b091768abbf61ea1e8d275
unknown <rakibulhasanridoy1@gmail.com>
2026-06-25 16:00:40 +0600

    feat: compose homepage by assembling all home sections in order

---
+++ app/(main)/page.jsx
@@ -0,0 +1,22 @@
+import Banner from '@/components/home/Banner'
+import WhyChooseUs from '@/components/home/WhyChooseUs'
+import FeaturedClasses from '@/components/home/FeaturedClasses'
+import LatestPosts from '@/components/home/LatestPosts'
+import Testimonials from '@/components/home/Testimonials'
+
+export const metadata = {
+  title: 'ApexForge | Home',
+  description: 'Transform your body and elevate your life with ApexForge fitness platform.',
+}
+
+export default function HomePage() {
+  return (
+    <>
+      <Banner />
+      <WhyChooseUs />
+      <FeaturedClasses />
+      <LatestPosts />
+      <Testimonials />
+    </>
+  )
+}
