65d0b5475d65466283b53b8d0aa0e85dbf20b7bd
unknown <rakibulhasanridoy1@gmail.com>
2026-06-27 09:33:40 +0600

    fix: Vercel production deployment has been fixed

--- next.config.mjs
+++ next.config.mjs
@@ -19,5 +19,14 @@ const nextConfig = {
       },
     ]
   },
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'}/api/:path*`,
+      },
+    ]
+  },
 }
 export default nextConfig
